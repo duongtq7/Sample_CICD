ff0f81ac55faa199d1b2d4812723a0d1bc2da8cb
duongtq7 <traanquangduong@gmail.com>
2024-12-23 23:44:32 +0700

    [Feat] Fix erros

--- backend/app/api/routers/students.py
+++ backend/app/api/routers/students.py
@@ -15,7 +15,7 @@ def get_db():
 
 @router.post("/students/", response_model=schemas.Student)
 def create_student(student: schemas.StudentCreate, db: Session = Depends(get_db)):
-    db_student = models.Student(**student.dict())
+    db_student = models(**student.dict())
     db.add(db_student)
     db.commit()
     db.refresh(db_student)
@@ -23,7 +23,7 @@ def create_student(student: schemas.StudentCreate, db: Session = Depends(get_db)
 
 @router.get("/students/{student_id}", response_model=schemas.Student)
 def read_student(student_id: int, db: Session = Depends(get_db)):
-    db_student = db.query(models.Student).filter(models.Student.id == student_id).first()
+    db_student = db.query(models).filter(models.id == student_id).first()
     if db_student is None:
         raise HTTPException(status_code=404, detail="Student not found")
     return db_student
